799971ababeb6d2f6ca73e93aff4ee906a19376f
Vlad <0986535843v@gmail.com>
2025-05-26 13:37:35 +0200

    ContactsPageUpgrade

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,14 @@ import { ProductsPage } from "./pages/ProductsPage";
 import { Navbar } from "./components/Navbar";
 import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react";
 import { AuthCallbackPage } from "./pages/AuthCallbackPage";
+import { ContactsPage } from "./pages/ContactsPage";
 
 export const App = () => {
   return (
     <>
       <Navbar />
       <Routes>
+        <Route path="/contacts" element={<ContactsPage />} />
         <Route path="/products" element={<ProductsPage />} />
         <Route
           path="/sso-callback"
