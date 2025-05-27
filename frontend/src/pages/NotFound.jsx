a99374f790a6e64d3c487a65fcd44c61ee15144d
EchoOFProgress <echoofprogress@gmail.com>
2025-05-27 10:05:19 +0200

    Future-HomePage In progress

---
+++ frontend/src/pages/NotFound.jsx
@@ -0,0 +1,27 @@
+import { div } from "framer-motion/client";
+
+
+
+export const NotFound = () =>{
+
+
+return(
+
+
+<div> 
+<p>Page Not Found</p>    
+    
+    
+    
+    
+    
+ </div>
+
+
+
+
+
+
+
+);
+};
